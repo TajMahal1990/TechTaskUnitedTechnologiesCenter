3d2a25985f981452f10909f983325f1251502d00
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-06-06 23:56:00 +0600

    add xml in home page

---
+++ app/src/main/java/com/example/bookpdf/data/model/BookListModel.kt
@@ -0,0 +1,5 @@
+/*
+package com.example.bookpdf.data.model
+
+data class BookListModel()
+*/
--- app/src/main/java/com/example/bookpdf/ui/book_list_activity/BookListActivity.kt
+++ app/src/main/java/com/example/bookpdf/ui/book_list_activity/BookListActivity.kt
@@ -1,11 +1,12 @@
-package com.example.bookpdf
+package com.example.bookpdf.ui.book_list_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookpdf.R
 
-class MainActivity : AppCompatActivity() {
+class BookListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_book_list)
     }
 }
