5f41577b660788c6893e35063556de51bcdac61b
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-06-07 17:34:57 +0600

    add xml category

---
+++ app/src/main/java/com/example/bookpdf/ui/categiry_activity/CategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookpdf.ui.categiry_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookpdf.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
