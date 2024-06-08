1521a22fdc2f99892ee53694902fada65db71304
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-06-09 00:46:51 +0600

    optimization of downloading

--- app/src/main/java/com/example/bookpdf/data/repository/BookRepo.kt
+++ app/src/main/java/com/example/bookpdf/data/repository/BookRepo.kt
@@ -3,23 +3,40 @@ package com.example.bookpdf.data.repository
 import android.annotation.SuppressLint
 import android.app.DownloadManager
 import android.content.Context
+import android.content.Context.DOWNLOAD_SERVICE
 import android.net.Uri
 import android.os.Environment
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.bookpdf.data.model.DownloadModel
 import com.example.bookpdf.domain.MyResponses
+import java.io.File
 
 class BookRepo(val context: Context) {
+
     private val downloadLd = MutableLiveData<MyResponses<DownloadModel>>()
     val downloadLiveData get() = downloadLd
     val TAG = "Details_Activity"
+
     @SuppressLint("Range")
     suspend fun downloadPdf(url: String, fileName: String) {
+        val file = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), fileName)
+        Log.i(TAG, "downloadPdf: ${file.absolutePath}")
+        if (file.exists()) {
+            val model = DownloadModel(
+                progress = 100,
+                isDownloaded = true,
+                downloadId = -1,
+                filePath = file.toURI().toString()
+            )
+            downloadLiveData.postValue(MyResponses.Success(model))
+            Log.i(TAG, "downloadPdf: File Already Exist")
+            return
+        }
 
         downloadLiveData.postValue(MyResponses.Loading())
 
-        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadManager = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
 
         val downloadRequest = DownloadManager.Request(Uri.parse(url)).apply {
             setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
@@ -30,10 +47,12 @@ class BookRepo(val context: Context) {
             setAllowedOverMetered(true)
             setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, fileName)
         }
+
         val downloadId = downloadManager.enqueue(downloadRequest)
 
         var isDownloaded = false
         var progress = 0
+
         while (!isDownloaded) {
             val cursor = downloadManager.query(DownloadManager.Query().setFilterById(downloadId))
             if (cursor.moveToNext()) {
@@ -48,15 +67,18 @@ class BookRepo(val context: Context) {
                                 cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
                             progress = ((downloadedBytesSize * 100L) / totalSize).toInt()
                             downloadLiveData.postValue(MyResponses.Loading(progress))
-
                         }
+
                     }
+
                     DownloadManager.STATUS_FAILED -> {
-                        val reason = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                        val reason =
+                            cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
                         isDownloaded = true
                         downloadLiveData.postValue(MyResponses.Error("Failed to Download $fileName.\nReason $reason"))
 
                     }
+
                     DownloadManager.STATUS_SUCCESSFUL -> {
                         progress = 100
                         isDownloaded = true
@@ -70,14 +92,11 @@ class BookRepo(val context: Context) {
                             filePath = filePath
                         )
                         downloadLiveData.postValue(MyResponses.Success(model))
-
                     }
-
                     DownloadManager.STATUS_PENDING -> {
                         downloadLiveData.postValue(MyResponses.Loading(progress))
                         Log.i(TAG, "downloadPdf: Pending")
                     }
-
                     DownloadManager.STATUS_PAUSED -> {
                         downloadLiveData.postValue(MyResponses.Loading(progress))
                         Log.i(TAG, "downloadPdf: Paused")
--- app/src/main/java/com/example/bookpdf/ui/details_activity/DetailsActivity.kt
+++ app/src/main/java/com/example/bookpdf/ui/details_activity/DetailsActivity.kt
@@ -3,15 +3,30 @@ package com.example.bookpdf.ui.details_activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.bookpdf.R
 import com.example.bookpdf.data.model.BooksModel
+import com.example.bookpdf.data.repository.BookRepo
 import com.example.bookpdf.databinding.ActivityDetailsBinding
+import com.example.bookpdf.domain.MyResponses
+import com.example.bookpdf.ui.details_activity.view_model.BookViewModel
+import com.example.bookpdf.ui.details_activity.view_model.BookViewModelFactory
 import com.example.bookpdf.ui.pdf_activity.PdfActivity
 import com.example.bookpdf.utils.loadOnline
 
 class DetailsActivity : AppCompatActivity() {
     val activity = this
     lateinit var binding: ActivityDetailsBinding
+
+    private val repo = BookRepo(activity)
+    private val viewModel by lazy {
+        ViewModelProvider(
+            activity,
+            BookViewModelFactory(repo)
+        )[BookViewModel::class.java]
+    }
+    private val TAG = "Details_Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +43,23 @@ class DetailsActivity : AppCompatActivity() {
 
             }
             mReadBookBtn.setOnClickListener {
-                val intent = Intent()
+                viewModel.downloadFile(bookModel.bookPDF, "${bookModel.title}.pdf")
 
-                intent.setClass(activity, PdfActivity::class.java)
-                startActivity(intent)
+                val intent = Intent()
+            }
+            viewModel.downloadLiveData.observe(activity) {
+                when (it){
+                    is MyResponses.Error -> {
+                        Log.e(TAG, "onCreate: ${it.errorMessage}")
+
+                    }
+                    is MyResponses.Loading -> {
+                        Log.i(TAG, "onCreate: Progress ${it.progress}")
+                    }
+                    is MyResponses.Success -> {
+                        Log.i(TAG, "onCreate: Downloaded ${it.data}")
+                    }
+                }
             }
         }
     }
